35fa5305e55fcf512fec864aa9a5047fb0131358
alexis gray <alexisjgray@yahoo.com>
2023-11-28 14:40:05 -0800

    working on get in test.js

--- intermediateLevel/packages/client/src/App.test.jsx
+++ intermediateLevel/packages/client/src/App.test.jsx
@@ -1,21 +1,24 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
 // Example: This checks that the app contains an h1 tag with the text "test all the things!"
 test('h1 tag contains text saying "Test all the things!"', () => {
-  render(<App />)
+  render(<App />);
 
-  const header = screen.getByText(/test all the things!/i)
+  const header = screen.getByText(/test all the things!/i);
 
-  expect(header).toBeInTheDocument()
-  expect(header.tagName).toEqual("H1")
-})
+  expect(header).toBeInTheDocument();
+  expect(header.tagName).toEqual("H1");
+});
 
 // Challenges:
 // Write code to test that clicking the button in App.js will display the secret message.
 // Check out App.js itself to see what the text should be.
-test('clicking the button shows the secret message', () => {
-  render(<App/>)
+test("clicking the button shows the secret message", () => {
+  render(<App />);
 
-  // Your code here
-})
+  const button = screen.getByRole("button");
+
+  expect(button).toBeInTheDocument();
+  expect(button.tagName).toEqual("BUTTON");
+});
--- intermediateLevel/packages/server/test.js
+++ intermediateLevel/packages/server/test.js
@@ -1,34 +1,71 @@
-import chai from 'chai'
-import chaiHttp from 'chai-http'
-import app from './app'
-import Hero from './models/hero.model'
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "./app";
+import Hero from "./models/hero.model";
 
-chai.should()
-chai.use(chaiHttp)
+chai.should();
+chai.use(chaiHttp);
 
-describe('Heroes', () => {
-  beforeEach((done) => { // Before each test we empty the database
-    Hero.deleteMany()
-      .then(() => done())
+describe("Heroes", () => {
+  beforeEach((done) => {
+    // Before each test we empty the database
+    Hero.deleteMany().then(() => done());
+  });
 
-  })
-
-
-  describe('/GET hero', () => { // Example: Tests that the heroes endpoint works, and returns nothing since we emptied the database prior
-    it('should get all the heroes', (done) => {
-      chai.request(app)
-        .get('/api/hero')
+  describe("/GET hero", () => {
+    // Example: Tests that the heroes endpoint works, and returns nothing since we emptied the database prior
+    it("should get all the heroes", (done) => {
+      chai
+        .request(app)
+        .get("/api/hero")
         .end((err, res) => {
-          res.should.have.status(200)
-          res.body.should.be.a('array')
-          res.body.length.should.be.eql(0)
-          done()
-        })
-    })
-  })
+          res.should.have.status(200);
+          res.body.should.be.a("array");
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
 
   // Challenges:
-  describe('/POST hero', () => {
-    // Your code here
-  })
-})
+  describe("/POST hero", () => {
+    it("should create a new hero", (done) => {
+      const newHeroData = {
+        name: "Superman",
+        power: "Strength",
+      };
+      chai
+        .request(app)
+        .post("/api/hero")
+        .send(newHeroData)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("name", "Superman");
+          done();
+        });
+    });
+  });
+
+  // const heroEndpoint = (heroId) => `/hero/${heroId}`;
+  describe("/GET /hero/:heroId", () => {
+    it("should retrieve a single hero's details", (done) => {
+      const newHeroData = {
+        name: "Superman",
+        id: 123,
+      };
+      chai
+        .request(app)
+        .get(`/hero/${newHeroData.id}`)
+        .end((err, res) => {
+          console.log(res);
+          console.log(res.body);
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("id").which.is.a("number");
+          res.body.should.have.property("id", 123);
+          done();
+        });
+    });
+  });
+});
