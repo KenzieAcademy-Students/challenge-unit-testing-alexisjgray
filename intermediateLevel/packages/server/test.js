f5e1bb1a59a9d7f6eafc09aaba51acb8bf71ce22
alexis gray <alexisjgray@yahoo.com>
2023-11-28 18:38:47 -0800

    finish

--- intermediateLevel/packages/server/test.js
+++ intermediateLevel/packages/server/test.js
@@ -2,6 +2,8 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 import app from "./app";
 import Hero from "./models/hero.model";
+import { request } from "http";
+const { expect } = require("chai");
 
 chai.should();
 chai.use(chaiHttp);
@@ -32,7 +34,6 @@ describe("Heroes", () => {
     it("should create a new hero", (done) => {
       const newHeroData = {
         name: "Superman",
-        power: "Strength",
       };
       chai
         .request(app)
@@ -47,24 +48,86 @@ describe("Heroes", () => {
     });
   });
 
-  // const heroEndpoint = (heroId) => `/hero/${heroId}`;
   describe("/GET /hero/:heroId", () => {
     it("should retrieve a single hero's details", (done) => {
       const newHeroData = {
         name: "Superman",
-        id: 123,
+      };
+
+      chai
+        .request(app)
+        .post("/api/hero")
+        .send(newHeroData)
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body).to.have.property("_id");
+
+          const heroId = res.body._id;
+
+          chai
+            .request(app)
+            .get(`/api/hero/${heroId}`)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.have.property("_id");
+              done();
+            });
+        });
+    });
+  });
+
+  describe("/DELETE /hero/:heroId", () => {
+    it("should delete a single hero's details", (done) => {
+      const newHeroData = {
+        name: "Superman",
       };
       chai
         .request(app)
-        .get(`/hero/${newHeroData.id}`)
+        .post("/api/hero")
+        .send(newHeroData)
         .end((err, res) => {
-          console.log(res);
-          console.log(res.body);
           res.should.have.status(200);
           res.body.should.be.a("object");
-          res.body.should.have.property("id").which.is.a("number");
-          res.body.should.have.property("id", 123);
-          done();
+          res.body.should.have.property("name", "Superman");
+
+          const heroId = res.body._id;
+
+          chai
+            .request(app)
+            .delete(`/api/hero/${heroId}`)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a("object");
+              done();
+            });
+        });
+    });
+  });
+
+  describe("/PUT /hero/:heroId", () => {
+    it("should update a single hero's details", (done) => {
+      const newHeroData = {
+        name: "Superman",
+      };
+
+      chai
+        .request(app)
+        .post("/api/hero")
+        .send(newHeroData)
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body).to.have.property("_id");
+
+          const heroId = res.body._id;
+
+          chai
+            .request(app)
+            .put(`/api/hero/${heroId}`)
+            .send({ name: "Bizarro" })
+            .end((err, res) => {
+              res.should.have.status(200);
+              done();
+            });
         });
     });
   });
